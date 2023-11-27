35e03f6b137108350f787d67637846356325c485
abodh <abodh.poudyal@gmail.com>
2023-11-26 20:55:20 -0800

    running version on IEEE-123 bus system

---
+++ main.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on: April 12, 2021
+@author: Abodh Poudyal
+"""
+
+import restoration as rs
+from fault_isolation import switch_identifier
+
+# list of fault locations -> single or multiple
+# python starts from 0
+fault_location = [63]
+
+# considers those cases in which fault occurs
+if not fault_location == []:
+
+    # obtain list of switches for fault isolation
+    op_sw_fi = switch_identifier(fault_location)
+
+    # restoration function that returns the following
+    loss_objective = rs.restoration(op_sw_fi)
+else:
+    loss_objective = 0
+
+print("Overall objective = ", loss_objective)
+
+
+
+
+
+
+
